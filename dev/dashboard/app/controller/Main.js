986276d3e84f00bf17e9ff2908fc00233c6d006c
Leo Liu <zeningliu@gmail.com>
2014-04-19 06:09:23 +0200

    change YQL to php

--- dev/dashboard/app/controller/Main.js
+++ dev/dashboard/app/controller/Main.js
@@ -43,42 +43,17 @@ function pad(n) { return ("0" + n).slice(-2); }
 function getData(){
         if (!stop){
 
+              $.ajax({
 
-            $("#data1").fadeOut('2000');
-            $("#data2").fadeOut('2000');
-            $("#data3").fadeOut('2000');
-            $("#data4").fadeOut('2000');
-            $("#data5").fadeOut('2000');
+              url: "proxy.php?url=https://arsnova.eu/statistics/",
 
-            $.ajax({
-              url: "http://query.yahooapis.com/v1/public/yql",
-              jsonp: "callback",
-              dataType: "jsonp",
-              data:{
-                  q: "select * from json where url=\"https://arsnova.eu/statistics/\"",
-                  format: "json"
-              },
-              async: false,
+              //async: false,
               success: function( response ){
-
-
-
-                $("#data1").text(response.query.results.json.activeUsers);
-
-                $("#data2").text(response.query.results.json.openSessions);
-                
-                $("#data3").text(response.query.results.json.closedSessions);
-
-                $("#data4").text(response.query.results.json.questions);
-                
-                $("#data5").text(response.query.results.json.answers);
-                
-
-                $("#data1").show('slow');
-                $("#data2").show('slow');
-                $("#data3").show('slow');
-                $("#data4").show('slow');
-                $("#data5").show('slow');
+                $("#data1").fadeOut('fast');
+                $("#data2").fadeOut('fast');
+                $("#data3").fadeOut('fast');
+                $("#data4").fadeOut('fast');
+                $("#data5").fadeOut('fast');
       
                 if ( data.length > 9) {
                         data.shift();
@@ -91,36 +66,47 @@ function getData(){
                     data.push
                     ( 
                         {time: time,
-                        activeUsers: response.query.results.json.activeUsers,
-                        openSessions: response.query.results.json.openSessions,
-                        closedSessions: response.query.results.json.closedSessions, 
-                        questions: response.query.results.json.questions, 
-                        answers: response.query.results.json.answers}
-
+                        activeUsers: response.contents.activeUsers,
+                        openSessions: response.contents.openSessions,
+                        closedSessions: response.contents.closedSessions, 
+                        questions: response.contents.questions, 
+                        answers: response.contents.answers}
                     ); 
                     
                     Ext.getCmp('Chart1')._axes[0]._minimum = 0;
-                    Ext.getCmp('Chart1')._axes[0]._maximum = parseInt(response.query.results.json.activeUsers) + 5;
+                    Ext.getCmp('Chart1')._axes[0]._maximum = parseInt(response.contents.activeUsers) + 5;
                     // Ext.getCmp('Chart1').redraw();
                     
-                    Ext.getCmp('Chart2')._axes[0]._minimum = parseInt(response.query.results.json.openSessions) - 10;
-                    Ext.getCmp('Chart2')._axes[0]._maximum = parseInt(response.query.results.json.openSessions) + 10;
+                    Ext.getCmp('Chart2')._axes[0]._minimum = parseInt(response.contents.openSessions) - 10;
+                    Ext.getCmp('Chart2')._axes[0]._maximum = parseInt(response.contents.openSessions) + 10;
                     // Ext.getCmp('Chart2').redraw();
 
-                    Ext.getCmp('Chart3')._axes[0]._minimum = parseInt(response.query.results.json.closedSessions) - 10;
-                    Ext.getCmp('Chart3')._axes[0]._maximum = parseInt(response.query.results.json.closedSessions) + 10;
+                    Ext.getCmp('Chart3')._axes[0]._minimum = parseInt(response.contents.closedSessions) - 10;
+                    Ext.getCmp('Chart3')._axes[0]._maximum = parseInt(response.contents.closedSessions) + 10;
                     // Ext.getCmp('Chart3').redraw();
 
-                    Ext.getCmp('Chart4')._axes[0]._minimum = parseInt(response.query.results.json.questions) - 10;
-                    Ext.getCmp('Chart4')._axes[0]._maximum = parseInt(response.query.results.json.questions) + 10;
+                    Ext.getCmp('Chart4')._axes[0]._minimum = parseInt(response.contents.questions) - 10;
+                    Ext.getCmp('Chart4')._axes[0]._maximum = parseInt(response.contents.questions) + 10;
                     // Ext.getCmp('Chart4').redraw();
 
-                    Ext.getCmp('Chart5')._axes[0]._minimum = parseInt(response.query.results.json.answers) - 10;
-                    Ext.getCmp('Chart5')._axes[0]._maximum = parseInt(response.query.results.json.answers) + 10;
+                    Ext.getCmp('Chart5')._axes[0]._minimum = parseInt(response.contents.answers) - 10;
+                    Ext.getCmp('Chart5')._axes[0]._maximum = parseInt(response.contents.answers) + 10;
                     // Ext.getCmp('Chart5').redraw();
 
                     Ext.getStore("ARSnovaVis").setData(data);
                     Ext.getStore("ARSnovaVis").load();
+
+                $("#data1").text(response.contents.activeUsers);
+                $("#data2").text(response.contents.openSessions);               
+                $("#data3").text(response.contents.closedSessions);
+                $("#data4").text(response.contents.questions);                
+                $("#data5").text(response.contents.answers);
+                
+                $("#data1").show('fast');
+                $("#data2").show('fast');
+                $("#data3").show('fast');
+                $("#data4").show('fast');
+                $("#data5").show('fast');
                     //console.log(response.query.results.json);
 
               }
